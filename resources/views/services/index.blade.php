ea70ac16c830e85081c14be8b2a0c68f03ab7601
Tim <forgamingaccsonly@gmail.com>
2023-04-06 21:36:26 +0800

    updated!

--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -8,25 +8,49 @@
 </head>
 <body>
 <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">Booking a Service</div>
+      <div class="row justify-content-center">
+            <div class="col-md-8">
+                 <div class="card">
+                    <div class="card-header">Booking a Service(s)</div>
                         <div class="card-body">
-                          <form action="#" method="POST">
-                            <label for="name">Name:</label>
-                            <input type="text" id="name" name="name" required>
-                            <br>
-                            <label for="email">Email:</label>
-                            <input type="email" id="email" name="email" required>
-                            <br>
-                            <label for="date">Date:</label>
+                          <form action="{{ route('login') }}" method="POST">
+                          <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address:') }}</label>
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name:') }}</label>
+                            <div class="col-md-6">
+                                <input id="name" type="name" class="form-control @error('name') is-invalid @enderror" name="name" required autocomplete="current-name">
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Date:') }}</label>
+                          <div class="col-md-6">
                             <input type="date" id="date" name="date" required>
-                            <br>
-                            <label for="time">Time:</label>
+                        </div>
+                          </div>
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Time:') }}</label>
+                          <div class="col-md-6">
                             <input type="time" id="time" name="time" required>
-                            <br>
-                            <label for="service">Service:</label>
+                        </div>
+                          </div>
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Service:') }}</label>
+                          <div class="col-md-6">
                             <select id="service" name="service" required>
                               <option value="">Select a service</option>
                               <option value="carpentry">Carpentry</option>
@@ -36,12 +60,22 @@
                               <option value="plumbing">Plumbing</option>
                               <option value="technician">Technician</option>
                             </select>
-                            <br>
-                            <label for="message">Message:</label>
-                            <textarea id="message" name="message"></textarea>
-                            <br>
-                            <input type="submit" value="Book It Now!">
+                          </div>
+                        </div>
+                        <div class="form-group row mb-0">
+                        <div class="col-md-8 offset-md-4">
+                        <button type="submit" class="btn btn-primary">
+                                    {{ __('Book It Now!') }}
+                                </button>
                           </form>
+                        </div>
+
+                        </div>
+                    </div>
+                  </div>
+            </div>
+      </div>
+</div>
 </body>
 </html>
 @endsection
