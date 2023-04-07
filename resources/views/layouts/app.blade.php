ee3aaf8127cc3897a6c4840d0ee86a8fa8add20f
Tim <forgamingaccsonly@gmail.com>
2023-04-08 02:40:08 +0800

    -push

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,12 +56,6 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Profile') }}
-                                    </a>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
